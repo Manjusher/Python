e9b09ad5b24fa5739ca8c6bc520b7584b8bf2ce1
root <manjush.elluru@gmail.com>
2021-08-04 19:57:45 +0530

    v3

--- practice.py
+++ practice.py
@@ -10,4 +10,10 @@ for i in range(6):
 	for j in range(6):
 		print(l[i][j],end="")
 	print()
-	
+l =[['*'] for i in range(6)]
+for i in range(6):
+        for j in range(i+1):
+                print(l[i][j],end="")
+        print()
+
+
