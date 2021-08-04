f62f594428b31f84be3a3ca77507e2ba9bb3e89a
root <manjush.elluru@gmail.com>
2021-08-04 18:34:03 +0530

    v2

--- practice.py
+++ practice.py
@@ -1,5 +1,13 @@
+
+l =[['*'] for i in range(6)]
+for i in range(6):
+	for j in range(i+1):
+		print(l[i][j],end="")
+	print()
+	
 l =[['*']*6 for i in range(6)]
 for i in range(6):
 	for j in range(6):
 		print(l[i][j],end="")
 	print()
+	
